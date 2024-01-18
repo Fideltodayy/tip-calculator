64ace340066efd2209098937c5ccd01f09b9c85e
Fideltodayy <fidelotieno11@gmail.com>
2024-01-18 12:01:22 +0300

    I DID IT MYSELFgit add .😎

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [bill, setBill] = useState();
+
+  const [youTip, setYouTip] = useState(0);
+  const [yourFriendsTip, setYourFriendsTip] = useState(0);
+
+  const pay = bill + (youTip / 100) * bill + (yourFriendsTip / 100) * bill;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill bill={bill} onBillChange={setBill} />
+      <Tip tip={youTip} onTipChange={setYouTip}>
+        {" "}
+        <label>How satisfied are you with the service??</label>
+      </Tip>
+      <Tip tip={yourFriendsTip} onTipChange={setYourFriendsTip}>
+        {" "}
+        <label>How satisfied is you're Friend with the service??</label>
+      </Tip>
+      <p>THe total pay is {pay}</p>
     </div>
   );
 }
 
-export default App;
+function Bill({ bill, onBillChange }) {
+  return (
+    <div>
+      <label>How much is the Bill?</label>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => onBillChange(Number(e.target.value))}
+      ></input>
+    </div>
+  );
+}
+
+function Tip({ children, tip, onTipChange }) {
+  return (
+    <div>
+      {children}
+      <select value={tip} onChange={(e) => onTipChange(e.target.value)}>
+        <option value={15}>Very much satisfied 15%</option>
+        <option value={10}>Little satisfied 10%</option>
+        <option value={5}>Little dissatisfied 5%</option>
+        <option value={0}>Very much dissatisfied 0%</option>
+      </select>
+    </div>
+  );
+}
